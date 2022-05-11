98759002d42a720fac028353256c1bcc3bc2016a
najadlaug <79115282+najadlaug@users.noreply.github.com>
2022-05-11 12:37:22 +0200

    HUD VIDA
    main amb hud vida perd vida amb tecla home

--- scenes/scripts/Global.gd
+++ scenes/scripts/Global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var lastpos = Vector2(30,230)
 
+var vida = 100
 var limit_left = -50
 var limit_top = -50
 var limit_right = 400
@@ -10,7 +11,7 @@ var limit_bottom = 385
 func principi_escena(n_escena, camera:Camera2D, personatge:KinematicBody2D):
 	match n_escena:
 		0:
-			limit_left = 10
+			limit_left = 3.5
 			limit_top = 0
 			limit_right = 1024
 			limit_bottom = 600
@@ -24,3 +25,22 @@ func principi_escena(n_escena, camera:Camera2D, personatge:KinematicBody2D):
 	camera.limit_top = limit_top
 	camera.limit_right = limit_right
 	camera.limit_bottom = limit_bottom
+
+
+func perd_vida(hp:AnimatedSprite):
+	vida -= 25
+	
+	if vida <= 0:
+		hp.frame = 4
+	elif vida <= 25:
+		hp.frame = 3
+	elif vida <= 50:
+		hp.frame = 2
+	elif vida <= 75:
+		hp.frame = 1
+	else:
+		hp.frame = 0
+	
+	
+	
+	

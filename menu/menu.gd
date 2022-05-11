ee466df786897d781211457fc3d3038cfe93e0f4
najadlaug <79115282+najadlaug@users.noreply.github.com>
2022-05-11 13:13:52 +0200

    main with menu

---
+++ menu/menu.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+func _on_settingsButton_pressed():
+	get_tree().change_scene("res://menu/scenes/main_settings.tscn")
+
+
+func _on_PlayBTN_pressed():
+	get_tree().change_scene("res://scenes/main.tscn")

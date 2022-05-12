ebcaaf13a35df2aedd3b1902efe41efa519a7f58
amontal15 <101257259+amontal15@users.noreply.github.com>
2022-05-12 08:55:25 +0200

    Update personatge_1.gd

--- Sprites personatges/personatge_1.gd
+++ Sprites personatges/personatge_1.gd
@@ -48,3 +48,14 @@ func _on_Area2D_body_entered(body):
 func _on_casa3_body_entered(body):
 	Global.lastpos = global_position + Vector2(0, 10)
 	get_tree().change_scene("res://scenes/escena_Casa3.tscn")
+<<<<<<< Updated upstream
+=======
+	
+func vida():
+	Global.perd_vida($HUD/AnimatedSprite)
+
+
+func _on_casa4_body_entered(body):
+	Global.lastpos = global_position + Vector2(0, 10)
+	get_tree().change_scene("res://scenes/escena_Casa4.tscn")
+>>>>>>> Stashed changes

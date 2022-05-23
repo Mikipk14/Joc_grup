5f836b3b7313722aa9dc308702347bb311db5751
najadlaug <79115282+najadlaug@users.noreply.github.com>
2022-05-23 13:45:33 +0200

    aaaa

--- personatges/Monstre_blau_petit/Monstre_blau_petit.gd
+++ personatges/Monstre_blau_petit/Monstre_blau_petit.gd
@@ -7,7 +7,16 @@ extends KinematicBody2D
 var Velocitat=1
 var velocitat_y = Vector2.DOWN
 var velocitat_x = Vector2.RIGHT
+var vida = 100 setget canvia_vida
+var viu = true
 
+func canvia_vida(nova_vida):
+	vida = nova_vida
+	$BarraVida.value = vida
+	if vida <= 0:
+		viu = false
+		print('MORT!---------------------')
+		$AnimatedSprite.play("mor")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,9 +24,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	anima(velocitat_x)
-	print(Node2D.get_personatge_position())
+	
 	
 func anima(velocitat_x):
+	if not viu:
+		return
 	if velocitat_x > Vector2(0,0):
 		$AnimatedSprite.play("corre")
 		$AnimatedSprite.flip_h = false
@@ -25,3 +36,10 @@ func anima(velocitat_x):
 		$AnimatedSprite.play("corre")
 		$AnimatedSprite.flip_h = true
 
+func hit(mal):
+	self.vida -= mal
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == 'mor':
+		queue_free()
--- sprites/Arma/Bala_Blava.gd
+++ sprites/Arma/Bala_Blava.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 var velocitat = Vector2(100, 0)
-
+var mal = 20
 
 func _physics_process(delta):
 	position += velocitat * delta
@@ -10,4 +10,6 @@ func _physics_process(delta):
 
 func _on_Bala_body_entered(body):
 #	body.queue_free()
+	if body.has_method('hit'):
+		body.hit(mal)
 	queue_free()
